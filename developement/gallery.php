866d4a97f2e5a0f36898e3438d819d92450f39b3
Kjeld Schmidt <superfluidmercury@live.de>
2014-06-24 19:16:44 +0200

    Developement/Stable mixup

--- developement/gallery.php
+++ developement/gallery.php
@@ -1,11 +1,10 @@
 <?php
+wp_register_style('screenGalleryStyle', plugins_url('css/galleryStyles.css', __FILE__));
+wp_enqueue_style('screenGalleryStyle');
 
-wp_register_style('screen-gallery-style', plugins_url('css/gallery-styles.css', __FILE__));
-wp_enqueue_style('screen-gallery-style');
-
-wp_register_script('screen-gallery-script', plugins_url('js/screen-gallery.js',  __FILE__));
-wp_enqueue_script('screen-gallery-script');
-wp_localize_script( 'screen-gallery-script', 'ajaxdata', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+wp_register_script('screenGalleryScript', plugins_url('js/screenGallery.min.js',  __FILE__));
+wp_enqueue_script('screenGalleryScript');
+wp_localize_script('screenGalleryScript', 'ajaxdata', array('ajaxurl' => admin_url('admin-ajax.php')));
 
 echo 	"<div class='hide'>
 			<span id='maxHeight'>". $screenOptions['max-height'] ."</span>
@@ -17,7 +16,7 @@ echo 	"<div class='hide'>
 <div class="galleryWrapper">
 	<div class="galleryBuffer">
 
-	<?php include('getImages.php'); getImages(); ?>
+		<?php include('getImages.php'); getImages(); ?>
 
 	</div>
 	<div class="gallery">

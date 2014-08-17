0a0fa1c1b310758d9988c6457f25aba4e8b21ac1
Kjeld Schmidt <superfluidmercury@live.de>
2014-08-18 00:35:45 +0200

    Extending Admin Pages
    Creating tab functionality and adding first example page.

--- developement/screenGalleryAdmin.php
+++ developement/screenGalleryAdmin.php
@@ -1,12 +1,26 @@
+<?php
+
+$overwiew_link = add_query_arg(array('screenGalleryTab'=>'overview'));
+$galleries_link = add_query_arg(array('screenGalleryTab'=>'galleries'));
+$tags_link = add_query_arg(array('screenGalleryTab'=>'tags'));
+$style_link = add_query_arg(array('screenGalleryTab'=>'style'));
+
+$page = (isset($_GET['screenGalleryTab'])) ? $_GET['screenGalleryTab'] : "overview";
+
+?>
+
 <div class="adminArea">
 	<nav class="tabs">
-		<div class="navTabs"><a href="">Overview</a></div>
-		<div class="navTabs"><a href="">Galleries</a></div>
-		<div class="navTabs"><a href="">Tags</a></div>
-		<div class="navTabs"><a href="">Style</a></div>
+		<div class="navTabs <?php if($page === "overview") echo "active" ?>"><a href="<?php echo $overwiew_link; ?>">Overview</a></div>
+		<div class="navTabs <?php if($page === "galleries") echo "active" ?>"><a href="<?php echo $galleries_link; ?>">Galleries</a></div>
+		<div class="navTabs <?php if($page === "tags") echo "active" ?>"><a href="<?php echo $tags_link; ?>">Tags</a></div>
+		<div class="navTabs <?php if($page === "style") echo "active" ?>"><a href="<?php echo $style_link; ?>">Style</a></div>
 	</nav>
+
 	<div class="adminContent">
-		<h2>What would you like to do?</h2>
+
+		<?php include 'admintabs/' . $page . '.php'; ?>
+		
 	</div>
 
 	

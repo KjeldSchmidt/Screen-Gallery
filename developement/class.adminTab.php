96df9862ac84697f636e4458d3c7e8264877aec8
Kjeld Schmidt <superfluidmercury@live.de>
2014-08-24 12:45:13 +0200

    Implemented WordPress-AdminTabs
    Implemented a generic, reusable file for tabs on option pages of
    plugins.

---
+++ developement/class.adminTab.php
@@ -0,0 +1,89 @@
+<?php
+
+class AdminTabs {
+
+	public $currentTab;
+	public $tabs = array();
+
+
+	function __construct($tabs) {
+		foreach ($tabs as $key => $value) {
+			$tab = new Tab($value);
+			$this->tabs[] = $tab;
+
+			if (isset($_GET['adminOptionTab'])) {
+				if ($tab->slug === $_GET['adminOptionTab']) $this->currentTab = $tab;
+			}
+		}
+
+		if (!isset($this->currentTab) $this->currentTab = $this->tabs[0];
+	}
+
+	function buildTabs() {
+		foreach ($this->tabs as $key => $value) {
+
+			$link = add_query_arg(array('adminOptionTab'=>$value->slug)); ?>
+
+			<div class="adminTabs <?php if ($this->currentTab === $value) echo "active"; ?>">
+				<a href="<?php echo $link; ?>">
+					<?php echo $value->name; ?>
+				</a>
+			</div> <?php
+		}
+
+		wp_register_style('adminTabsStyle', plugins_url('css/adminTabsStyle.css', __FILE__));
+		wp_enqueue_style('AdminTabsStyle');
+	}
+
+	function assignContent() {
+		$url = "adminTabs/" . $this->currentTab->file;
+
+		if (is_file($url)) include $url;
+		else echo $file . " could not be included. Please check file name, location and adminTabs configuration."
+	}
+
+}
+
+class Tab {
+	
+	public $name;
+	public $slug;
+	public $file;
+
+
+	function __construct($tabBasis) {
+		if(is_array($tabBasis)) {
+			if (isset($tabBasis[0]))	$this->name = $tabBasis[0];
+			if (isset($tabBasis[1]))	$this->slug = $tabBasis[1];
+			if (isset($tabBasis[2]))	$this->file = $tabBasis[2];
+
+			if(!isset($this->slug))		guessFromName($this->name);
+
+			if(!isset($this->file))		$this->file = $this->slug;
+
+		} else guessFromName($tabBasis);
+
+		$this->file .= '.php';
+	}
+
+	function guessFromName($name) {
+		$this->name	= $name;
+		$this->slug	= makeSlug($name);
+		$this->file = $this->slug;
+	}
+
+	function makeSlug($text) {
+		$text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+		$text = trim($text, '-');
+		$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+		$text = strtolower($text);
+		$text = preg_replace('~[^-\w]+~', '', $text);
+
+		if (empty($text)) {	return 'n-a'; }
+
+  		return $text;
+	}
+	
+}
+
+?>
--- developement/screenGalleryAdmin.php
+++ developement/screenGalleryAdmin.php
@@ -1,37 +1,24 @@
 <?php
 
-$overwiew_link = add_query_arg(array('screenGalleryTab'=>'overview'));
-$galleries_link = add_query_arg(array('screenGalleryTab'=>'galleries'));
-$tags_link = add_query_arg(array('screenGalleryTab'=>'tags'));
-$style_link = add_query_arg(array('screenGalleryTab'=>'style'));
+include 'class.adminTab.php';
 
-$page = (isset($_GET['screenGalleryTab'])) ? $_GET['screenGalleryTab'] : "overview";
+$tabs = array(
+	'Overview',
+	'Galleries',
+	'Tags',
+	'Style'
+);
+
+$tabs = new AdminTabs($tabs);
 
 ?>
 
 <div class="adminArea">
 	<nav class="tabs">
-		<div class="navTabs <?php if($page === "overview") echo "active" ?>"><a href="<?php echo $overwiew_link; ?>">Overview</a></div>
-		<div class="navTabs <?php if($page === "galleries") echo "active" ?>"><a href="<?php echo $galleries_link; ?>">Galleries</a></div>
-		<div class="navTabs <?php if($page === "tags") echo "active" ?>"><a href="<?php echo $tags_link; ?>">Tags</a></div>
-		<div class="navTabs <?php if($page === "style") echo "active" ?>"><a href="<?php echo $style_link; ?>">Style</a></div>
+		<?php $tabs->buildTabs(); ?>
 	</nav>
 
 	<div class="adminContent">
-
-		<?php include 'admintabs/' . $page . '.php'; ?>
-		
+		<?php $tabs->assignContent(); ?>
 	</div>
-
-	
-
 </div>
-
-
-<?php
-
-wp_register_style('screenGalleryAdminStyle', plugins_url('css/galleryAdminStyles.css', __FILE__));
-wp_enqueue_style('screenGalleryAdminStyle');
-
-
-?>

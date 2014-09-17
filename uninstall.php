5c57de10a9c6eb1816bb90087e15c6111df22115
Kjeld Schmidt <superfluidmercury@live.de>
2014-09-17 16:43:57 +0200

    Database gets removed on uninstall

--- uninstall.php
+++ uninstall.php
@@ -5,4 +5,9 @@ if (!defined( 'WP_UNINSTALL_PLUGIN'))
 
 delete_option('screen_gallery');
 
+global $wpdb;
+$table_name = $tables_galleries_name;
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}screengallery_galleries" );
+$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}screengallery_relation"  );
+
 ?>
